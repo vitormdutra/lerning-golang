21d36b2ad91ca7413ffd4ce880706866b94c00ce
Vitor Marcus Dutra <ntw1942@gmail.com>
2024-01-01 19:30:47 -0300

    init course

---
+++ start.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	x := 10
+	y := "test"
+	fmt.Printf("x: %v, %T,\n", x, x)
+	fmt.Printf("y: %v, %T,\n\n\n", y, y)
+	numerodebytes, erros := fmt.Print("Hello World!")
+	fmt.Println(numerodebytes, erros)
+
+	x = 20
+	fmt.Printf("x: %v, %T,\n", x, x)
+
+}
